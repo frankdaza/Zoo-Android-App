191dd98b66305944a8ab749423209bb3c0512805
Frank Edward Daza González <frank.daza2@gmail.com>
2018-02-18 14:35:50 -0500

    My first ZooApp commit

---
+++ app/src/main/java/com/frankdaza/zooapp/model/Animal.kt
@@ -0,0 +1,17 @@
+package com.frankdaza.zooapp.model
+
+/**
+ * Created by Frank Edward Daza González on 2/18/18.
+ */
+class Animal {
+
+    var name: String
+    var description: String
+    var image: Int
+
+    constructor(name: String, description: String, image: Int) {
+        this.name = name
+        this.description = description
+        this.image = image
+    }
+}
